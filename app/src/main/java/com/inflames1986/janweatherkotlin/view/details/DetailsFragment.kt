9f8ca26516e47b3ddb830260eebc6917b579d847
TMichael <js5859168@gmail.com>
2022-04-26 13:23:20 +0300

    Coil, Glide, Picasso - с включенным на железе прокси все работает, кое какие корректировки

--- app/src/main/java/com/inflames1986/janweatherkotlin/view/details/DetailsFragment.kt
+++ app/src/main/java/com/inflames1986/janweatherkotlin/view/details/DetailsFragment.kt
@@ -4,11 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import coil.ImageLoader
+import coil.decode.SvgDecoder
 import coil.load
+import coil.request.ImageRequest
 import com.bumptech.glide.Glide
+//import coil.load
+//import com.bumptech.glide.Glide
 import com.google.android.material.snackbar.Snackbar
 import com.inflames1986.janweatherkotlin.R
 import com.inflames1986.janweatherkotlin.databinding.FragmentDetailsBinding
@@ -16,6 +22,7 @@ import com.inflames1986.janweatherkotlin.model.entities.Weather
 import com.inflames1986.janweatherkotlin.utils.KEY_BUNDLE_WEATHER
 import com.inflames1986.janweatherkotlin.viewmodel.DetailsState
 import com.inflames1986.janweatherkotlin.viewmodel.DetailsViewModel
+import com.squareup.picasso.Picasso
 
 
 class DetailsFragment : Fragment() {
@@ -75,18 +82,31 @@ class DetailsFragment : Fragment() {
                     Snackbar.make(mainView, "Получилось", Snackbar.LENGTH_LONG)
                         .show()
 
-                    Glide.with(requireContext())
-                        .load("https://freepngimg.com/thumb/city/36275-3-city-hd.png")
-                        .into(headerCityIcon)
+//                    Glide.with(requireContext())
+//                        .load("https://freepngimg.com/thumb/city/36275-3-city-hd.png")
+//                        .into(headerCityIcon)
 
 //                    Picasso.get()?.load("https://freepngimg.com/thumb/city/36275-3-city-hd.png")
-//                         ?.into(headerIcon)
+//                         ?.into(headerCityIcon)
 
-//                    headerCityIcon.load("https://freepngimg.com/thumb/city/36275-3-city-hd.png")
-//                    icon.loadSvg("https://yastatic.net/weather/i/icons/blueye/color/svg/${weather.icon}.svg")
+                    headerCityIcon.load("https://freepngimg.com/thumb/city/36275-3-city-hd.png")
+                    icon.loadSvg("https://yastatic.net/weather/i/icons/blueye/color/svg/${weather.icon}.svg")
                 }
             }
         }
+    }
+
+    fun ImageView.loadSvg(url:String){
+        val imageLoader = ImageLoader.Builder(this.context)
+            .componentRegistry { add(SvgDecoder(this@loadSvg.context)) }
+            .build()
+        val request = ImageRequest.Builder(this.context)
+            .crossfade(true)
+            .crossfade(500)
+            .data(url)
+            .target(this)
+            .build()
+        imageLoader.enqueue(request)
     }
 
     companion object {
