c423a1df201406749139a6c7f0e41f4e8344b746
TMichael <js5859168@gmail.com>
2022-05-10 15:08:01 +0300

    Добавил новую кнопку для работы с Location, разрешения, локэйшн-листенер по времени и расстоянию и начал работать с геокодером - создали функцию getAddressByLocation

--- app/src/main/java/com/inflames1986/janweatherkotlin/view/main/WeatherListFragment.kt
+++ app/src/main/java/com/inflames1986/janweatherkotlin/view/main/WeatherListFragment.kt
@@ -1,16 +1,27 @@
 package com.inflames1986.janweatherkotlin.view.main
 
+import android.Manifest
+import android.annotation.SuppressLint
 import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Geocoder
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
+import androidx.core.content.ContextCompat
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import com.google.android.material.snackbar.Snackbar
 import com.inflames1986.janweatherkotlin.R
 import com.inflames1986.janweatherkotlin.databinding.FragmentWetherListBinding
+import com.inflames1986.janweatherkotlin.model.entities.City
 import com.inflames1986.janweatherkotlin.model.entities.Weather
 import com.inflames1986.janweatherkotlin.utils.DataSetRus
 import com.inflames1986.janweatherkotlin.utils.KEY_BUNDLE_WEATHER
@@ -20,6 +31,7 @@ import com.inflames1986.janweatherkotlin.view.details.DetailsFragment
 import com.inflames1986.janweatherkotlin.viewmodel.AppState
 import com.inflames1986.janweatherkotlin.viewmodel.MainViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
+import java.util.*
 
 class WeatherListFragment : Fragment() {
 
@@ -60,7 +72,10 @@ class WeatherListFragment : Fragment() {
 
         binding.fragmentWetherListRecyclerView.adapter = adapter
         binding.fragmentWetherListButton.setOnClickListener {
+
             changeWeatherDataSet()
+            setupFabLocation()
+
             val sharedPreferences =
                 requireActivity().getSharedPreferences(KEY_SP_FILE_NAME_1, Context.MODE_PRIVATE)
             val editor = sharedPreferences.edit()
@@ -71,6 +86,153 @@ class WeatherListFragment : Fragment() {
         viewModel.getLiveData().observe(viewLifecycleOwner, observer)
         viewModel.getWeatherFromLocalSourceRus()
     }
+
+    private fun setupFabLocation() {
+        binding.mainFragmentFABLocation.setOnClickListener {
+            checkPermission()
+        }
+    }
+
+    private fun checkPermission() {
+        // а есть ли разрешение?
+        if (ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            getLocation()
+        } else if (shouldShowRequestPermissionRationale(Manifest.permission.READ_CONTACTS)) {
+            // важно написать убедительную просьбу
+            explain()
+        } else {
+            mRequestPermission()
+        }
+    }
+
+    private fun explain() {
+        AlertDialog.Builder(requireContext())
+            .setTitle(resources.getString(R.string.dialog_rationale_title))
+            .setMessage(resources.getString(R.string.dialog_rationale_message))
+            .setPositiveButton(resources.getString(R.string.dialog_rationale_give_access)) { _, _ ->
+                mRequestPermission()
+            }
+            .setNegativeButton(getString(R.string.dialog_rationale_decline)) { dialog, _ -> dialog.dismiss() }
+            .create()
+            .show()
+    }
+
+    private val REQUEST_CODE = 997
+    private fun mRequestPermission() {
+        requestPermissions(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), REQUEST_CODE)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == REQUEST_CODE) {
+            for (i in permissions.indices) {
+                if (permissions[i] == Manifest.permission.ACCESS_FINE_LOCATION && grantResults[i] == PackageManager.PERMISSION_GRANTED) {
+                    getLocation()
+                } else {
+                    explain()
+                }
+            }
+        } else {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        }
+    }
+
+    fun getAddressByLocation(location: Location){
+        //val geocoder = Geocoder(requireContext())
+        val geocoder = Geocoder(requireContext(), Locale.getDefault())
+        val timeStump = System.currentTimeMillis()
+        Thread{
+            val addressText = geocoder.getFromLocation(location.latitude,location.longitude,1000000)[0].getAddressLine(0)
+            requireActivity().runOnUiThread {
+                showAddressDialog(addressText,location)
+            }
+        }.start()
+        Log.d("@@@"," прошло ${System.currentTimeMillis() - timeStump}")
+    }
+
+    private val locationListenerTime = object : LocationListener {
+        override fun onLocationChanged(location: Location) {
+            Log.d("@@@",location.toString())
+            getAddressByLocation(location)
+        }
+        override fun onProviderDisabled(provider: String) {
+            super.onProviderDisabled(provider)
+        }
+        override fun onProviderEnabled(provider: String) {
+            super.onProviderEnabled(provider)
+        }
+
+    }
+
+    private val locationListenerDistance = object : LocationListener {
+        override fun onLocationChanged(location: Location) {
+            Log.d("@@@",location.toString())
+            getAddressByLocation(location)
+        }
+        override fun onProviderDisabled(provider: String) {
+            super.onProviderDisabled(provider)
+        }
+        override fun onProviderEnabled(provider: String) {
+            super.onProviderEnabled(provider)
+        }
+
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun getLocation(){
+        context?.let {
+            val locationManager = it.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+            if(locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)){
+                val providerGPS = locationManager.getProvider(LocationManager.GPS_PROVIDER) // можно использовать BestProvider
+                /*providerGPS?.let{
+                    locationManager.requestLocationUpdates(
+                        LocationManager.GPS_PROVIDER,
+                        10000L,
+                        0f,
+                        locationListenerTime
+                    )
+                }*/
+                providerGPS?.let{
+                    locationManager.requestLocationUpdates(
+                        LocationManager.GPS_PROVIDER,
+                        0,
+                        100f,
+                        locationListenerDistance
+                    )
+                }
+            }
+        }
+    }
+
+    private fun showAddressDialog(address: String, location: Location) {
+        activity?.let {
+            AlertDialog.Builder(it)
+                .setTitle(getString(R.string.dialog_address_title))
+                .setMessage(address)
+                .setPositiveButton(getString(R.string.dialog_address_get_weather)) { _, _ ->
+                    //onItemClick(
+                        Weather(
+                            City(
+                                address,
+                                location.latitude,
+                                location.longitude
+                            )
+                        )
+                    //)
+                }
+                .setNegativeButton(getString(R.string.dialog_button_close)) { dialog, _ -> dialog.dismiss() }
+                .create()
+                .show()
+        }
+    }
+
 
     private fun changeWeatherDataSet() {
         if (isDataSetRus) {
